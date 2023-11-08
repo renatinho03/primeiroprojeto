355a64d33026276001c49912e714d693fbff6e79
Renato Henrique dos Santos Pereira <100329737+renatinho03@users.noreply.github.com>
2023-11-08 17:12:17 -0300

    Add files via upload

---
+++ primeiroprojeto/src/PrimeiroProjeto.java
@@ -0,0 +1,8 @@
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!!");
+        System.out.println("Meu nome é Renato Henrique");
+        System.out.println("Tenho 20 anos!!");
+        System.out.println("Muito prazer em te conhecer!!");
+    }
+}
